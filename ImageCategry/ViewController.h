b51d3baf51cb6dfd9ef45974d810cec682a5f051
fanchuan <fanchuan@9hcom.com>
2017-06-20 14:50:06 +0800

    Initial Commit

---
+++ ImageCategry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageCategry
+//
+//  Created by fanchuan on 2017/6/20.
+//  Copyright © 2017年 fanchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageCategry/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImageCategry
+//
+//  Created by fanchuan on 2017/6/20.
+//  Copyright © 2017年 fanchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
